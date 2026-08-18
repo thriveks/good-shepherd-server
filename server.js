af305215eb6c577961b659b43655b75978bfd2b2
thriveks <dev@thriveks.com>
2026-08-18 09:22:53 -0500

    Allow Command Center assignment CORS headers

--- server.js
+++ server.js
@@ -78,7 +78,10 @@ app.use((req, res, next) => {
 
     if (allowedOrigins.includes(origin)) {
         res.header("Access-Control-Allow-Origin", origin);
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-webhook-secret");
+        res.header(
+  "Access-Control-Allow-Headers",
+  "Origin, X-Requested-With, Content-Type, Accept, x-webhook-secret, x-app-build, x-app-version, x-app-client"
+);
         res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
     }
 
